885953cb5b8c05d8c2b3cb349cee67499077acbc
umeraziz45 <88170471+umeraziz45@users.noreply.github.com>
2023-04-16 19:27:21 -0600

    added counter for score

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,82 @@ import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzie, setTenzie] = useState(false);
+  const [counter, setCounter] = useState(0);
 
-  const [dice, setDice] = useState(createNewDice());
-  const [tenzies, setTenzies] = useState(false);
-
-  useEffect( () => {
-    console.log('change');
+  useEffect( () =>{
+    console.log(dice, 'changed');
     const allHeld = dice.every( elem => elem.isHeld);
     const firstValue = dice[0].value;
-    const allSameValue = dice.every( elem => elem.value === firstValue);
-    if(allSameValue && allHeld) {
-      console.log('you win');
-      setTenzies(true);
+    const everyValue = dice.every( elem => elem.value == firstValue);
+    if(allHeld && everyValue) {
+      setTenzie(true);
+      console.log('you win!');
     }
-  }, [dice])
+  },[dice])
 
-  function createNewDice (){
+  function allNewDice(){
     const diceArr = [];
-
     for(let i = 0; i < 10; i++){
       diceArr.push({
-        value:Math.ceil(Math.random() * 6),
-        isHeld: false,
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false, 
         id: nanoid()
-      }
-        );
+      })     
     }
-
     return diceArr;
   }
 
-  
-  const rollDice = () => {
-    if(tenzies){
-      setTenzies(false)
-      setDice(createNewDice);
+  const diceHtml = dice.map( elem => {
+   return <Dice 
+   id={elem.id}
+   value={elem.value}
+   isHeld={elem.isHeld}
+   holdDice={() => holdDice(elem.id)}
+    />
+  })
+
+  function diceRoll() {
+    if(tenzie){
+      setDice(allNewDice); setTenzie(false);
+      setCounter(0);
     } else {
-      setDice( prevArray =>  prevArray.map( elem =>  {
-        return elem.isHeld ? 
-          elem :
-          {
-            value:Math.ceil(Math.random() * 6),
-            isHeld: false,
-            id: nanoid() 
-          }
+      setDice(prevArray => prevArray.map( elem => {
+      return  elem.isHeld ?
+        elem :
+        {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false, 
+          id: nanoid()
         }
-      ));
+      }));
+      setCounter(prevCount => prevCount + 1);
     }
   }
-  
-  const clickDice = id => {
-   setDice(oldDice => oldDice.map( elem => {
-    return elem.id == id ? 
-      {...elem, isHeld: !elem.isHeld} :
-      elem
-    })
-   ) 
+
+  function holdDice(id) {
+    setDice(prevArray => prevArray.map(elem => {
+       return elem.id == id ? 
+       {...elem, isHeld: !elem.isHeld} :
+       elem
+     })
+    );
   }
-  
-  const diceHtml = dice.map( elem => {
-    return <Dice key={elem.id} value={elem.value} isHeld={elem.isHeld}  clickDice={() => clickDice(elem.id)} />
-  });
-  console.log(tenzies)
 
   return (
     <div className='App'>
-      {tenzies && < Confetti/>}
+      {tenzie && <Confetti />}
       <div className="diceContainer">
         {diceHtml}
       </div>
-      <button onClick={rollDice} className='diceBtn'>{tenzies ? 'New Game' : 'Roll'}</button>
-      
+      <button className='diceBtn' onClick={diceRoll}>{tenzie ? 'New Game' : 'Roll'}</button>
+      <div className="score">
+        <p>Score:</p>
+        <div className="counter">
+        {counter}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 export default function Dice (props) {
   return (
-    <div className={
-      props.isHeld ?
-      'clicked' :
-      'dice'
-    }
-    onClick={props.clickDice}
-    >
+    <div className={props.isHeld ? 
+    'clicked' : 
+    "dice"} onClick={props.holdDice}
+    key={props.id}>
       <h2 >{props.value}</h2>
     </div>
   )
